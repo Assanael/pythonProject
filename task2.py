3ccb64a832dfedcdc06889c424d16557c0b7125f
Assanael <99356173+Assanael@users.noreply.github.com>
2022-08-23 10:23:51 +0300

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,8 +1,8 @@
 p = int(input('Введите число от 1 до 100: '))
 for p in range(1, 101):
-        if p == 1:
+        if p % 10 == 1 and p % 100 != 11:
             print(f"{p} процент")
-        elif 1 < p < 5:
+        elif 1 < p % 10 < 5 and not 11 < p % 100 < 15:
             print(f"{p} процента")
         else:
             print(f"{p} процентов")
